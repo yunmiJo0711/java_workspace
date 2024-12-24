7d73beb53363c20fe065f4bce5d7abb231604db9
yunmijo <joyunmi66@naver.com>
2024-12-24 18:13:26 +0900

    day01숙제

---
+++ partA/src/basic/day01/A05ForTest.java
@@ -0,0 +1,17 @@
+package basic.day01;
+
+// 숙제
+public class A05ForTest {
+    // javascript for문을 그대로 적용
+    //              ㄴ for(i=0;i<100;i++){   }
+
+    public static void main(String[] args) {
+        // 1부터 100까지 더하는 for문을 찾아서 변형하시면 됩니다. 
+        long result = sumNtoM(4,10);
+        System.out.println("sumNtoM(4,10) = " + result);
+
+    }
+
+    // 메소드 정의 sumNtoM(4,10) => 4부터 10까지 더하기
+    
+}
