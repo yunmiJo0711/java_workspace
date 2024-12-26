42159f8cfa933760ef7f5692091ba9843f4a5a35
yunmijo <joyunmi66@naver.com>
2024-12-26 17:56:53 +0900

    day last

--- partA/src/basic/day02/A14StringInput.java
+++ partA/src/basic/day02/A14StringInput.java
@@ -18,7 +18,8 @@ public class A14StringInput {
         System.out.println("입력한 아이디 확인 : " + userid);
       }
       System.out.print("계속하시겠습니까 (Y/N) ?");
-      if (sc.nextLine().toUpperCase().equals("N"))
+      if (sc.nextLine().toUpperCase().equals("N"))  // N 이면
+      // if (!sc.nextLine().toUpperCase().equals("Y"))  // Y 가 아니면
           run=false;
     }
     // 단, 계속하시겠습니까 (Y/N) ? 로 확인 입력을 받고 N(또는 n) 입력하면 while 종료.
