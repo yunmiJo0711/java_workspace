1e5359e82106852b17dcd13d791d0dc4a66a9272
yunmijo <joyunmi66@naver.com>
2024-12-31 14:11:57 +0900

    day5 인스턴스 메소드

--- partB/src/object/day05/B03MyMathTest.java
+++ partB/src/object/day05/B03MyMathTest.java
@@ -10,11 +10,18 @@ public class B03MyMathTest {
         System.out.println("myMath.add() : " + myMath.add());  // 690
         System.out.println("myMath.substract() : " + myMath.substract());  // -222
 
+        System.out.println("=== print 메소드 : type 에 따라 +,-,* 중에서 연산 선택하고 출력 === ");
         myMath.print(1); // 234 + 456 = 690
         myMath.print(2); // 234 - 456 = -222
-        myMath.print(3); // 234 * 456 = ....
+        myMath.print(3); // 234 * 456 = 106704
 
-        MyMath temp = new MyMath();
+        System.out.println("=== 메소드 호출할 때 연산 선택하면 문자열 리턴 ===");
+        System.out.println(myMath.mathToString(myMath.ADD));
+        System.out.println(myMath.mathToString(myMath.SUB));
+        System.out.println(myMath.mathToString(myMath.MUL));
+
+        System.out.println("=== 초기값 테스트 ===");
+        MyMath temp = new MyMath();  // first, second 필드값이 처음부터 0으로 저장. 
         System.out.println("temp.add() : " + temp.add());  // 0
         System.out.println("temp.add() : " + temp.substract());  // 0
         
--- partB/src/object/day05/MyMath.java
+++ partB/src/object/day05/MyMath.java
@@ -8,7 +8,13 @@ public class MyMath {
     int second;
     int result;   // 사용 보류
 
+    // 상수 static final 활용
+    static final int ADD = 1;
+    static final int SUB = 2;
+    static final int MUL = 3;
+    
     // 인스턴스 메소드 정의 : 생성된 객체의 데이터(인스턴스 변수)를 다루는 메소드 
+    // 유형 1 : 각각의 연산을 실행한 결과로 리턴값을 전달하는 메소드
     public int add(){
         return first + second;
     }
@@ -21,27 +27,58 @@ public class MyMath {
         return first * second;
     }
 
-
+    // 유형 2 : 인자로 전달된 타입으로 연산을 선택하여 실행하고 출력하는 메소드. 리턴없음
     public void print(int type){
         // switch 문을 이용하여 type 1,2,3 각각에 대해
         // 2 + 3 = 5 또는 2 - 3 = -1 또는 2 * 3 = 6 과 같이 출력해보기
         // -> print 메소드에서 add(), substract(), multiply() 메소드를 호출하여 결과값 구합니다. 
         switch (type) {
             case 1:
-                System.out.println(String.format("null", null));
+            // 234 + 456 = 690
+                System.out.println(String.format("%d + %d =%d", this.first,this.second,add()));
                 
                 break;
             case 2:
-                System.out.println(String.format("null", null));
+            // 234 - 456 = -222
+                System.out.println(String.format("%d - %d =%d", this.first,this.second,substract()));
                     
                 break;
             case 3:
-                System.out.println(String.format("null", null));
+            // 234 * 456 = 106704
+                System.out.println(String.format("%d x %d =%d", this.first,this.second,multiply()));
                         
                 break;
             default:
-
+                System.out.println("잘못된 타입입니다.");
                 break;
         }
     }
+
+
+    // 유형 3 : 유형 2와 동작은 유사. 단, 문자열 리턴값을 갖고 static final 상수 활용.
+    public String mathToString(int type){
+        String result;
+        switch (type) {
+            case ADD:  // 정수값 1 비교
+            // 234 + 456 = 690
+                result = String.format("%d + %d =%d", this.first,this.second,add());
+                
+                break;
+            case SUB:
+            // 234 - 456 = -222
+                result = String.format("%d - %d =%d", this.first,this.second,substract());
+                    
+                break;
+            case MUL:
+            // 234 * 456 = 106704
+                result = String.format("%d x %d =%d", this.first,this.second,multiply());
+                        
+                break;
+            default:
+               result = "잘못된 type 입니다.";
+                break;
+        }
+        return result;
+    }
+
 }
