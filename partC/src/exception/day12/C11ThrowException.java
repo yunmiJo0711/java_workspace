0df99fad58ae6d635e8530422476cae41639c6f3
yunmijo <joyunmi66@naver.com>
2025-01-10 14:55:01 +0900

    day12입출력

--- partC/src/exception/day12/C11ThrowException.java
+++ partC/src/exception/day12/C11ThrowException.java
@@ -3,33 +3,43 @@ package exception.day12;
 // 예외 발생하기
 public class C11ThrowException {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IllegalAgeException {
 
         // 예외 처리 방법 1
         try {
             new C11ThrowException().checkAge(20);
             new C11ThrowException().checkAge(13);
-
         } catch (Exception e) {
             System.out.println("예외 : " + e.getMessage());
         }
-        // 예외 처리 방법 2
-        new C11ThrowException().checkAge(13);
+
+
+        // 예외 처리 방법 2 : throws 로 해결 
+        new C11ThrowException().checkAge(11);
+        // jvm 으로 익셉션 처리를 위임. -> jvm 은 오류 메시지 출력 -> 기본 동작
+        // main 에서는 try ~ catch로 프로그램 내에서 처리되도록 합니다. 
 
         System.out.println("프로그램 종료합니다.");
-
     }
 
-    public void checkAge(int age) throws Exception{  // 체크 익셉션 메소드로 정의 
+    // 메소드의 throws 하는 예외는 RuntimeException (언체크 익셉션) 은 적용이 안됩니다. 
+    public void checkAge(int age) throws IllegalAgeException{  // 체크 익셉션 메소드로 정의 
         // 예시 : 성인 나이 값으로 20세 미만은 예외 발생하기
         if(age < 20){
-            // 비정상적인 값이므로 예외 발생 : 클래스는 IllegalArgumentException, Exception 등
-            // 내용에 맞는 것으로 선택합니다. 
-            throw new IllegalArgumentException("잘못된 나이 입력값입니다.");
+            // 비정상적인 값이므로 예외 발생 : 새로운 익셉션으로 예외 발생
+            throw new IllegalAgeException("잘못된 나이 입력값입니다.");
         }else {
             System.out.println("정상적인 age 값 입니다. ");
         }
 
     }
 
+}
+// 새로운 Exception 을 정의 
+class IllegalAgeException extends Exception{
+    // 커스텀 생성자 재정의 : 메시지 설정
+    IllegalAgeException(String message){
+        super(message);
+
+    }
 }
---
+++ partC/src/io/day12/C12StandertIOTest.java
@@ -0,0 +1,72 @@
+package io.day12;
+
+import java.io.IOException;
+import java.security.PublicKey;
+
+public class C12StandertIOTest {
+
+    // 표준 입력 스트림 : 콘솔(키보드) -> 입력버퍼 -> 메모리(프로그램)
+    // 표준 출력 스트림 : 메모리(프로그램) -> 출력버퍼 -> 콘솔(화면장치)
+    // System.in 은 InputStream , System.out 은 PrintStream 타입.
+
+    // 주의 : 아스키코드 문자는 1바이트로 처리, UTF-8 한글은 2바이트로 처리
+    public static void main(String[] args) {
+
+        // 표준 입력과 출력
+
+        try {
+
+            stdioTest1();
+       
+        } catch (IOException e) {
+           System.out.println("예외 : " + e.getMessage());
+        }
+
+    } //  main end
+
+     // 반복문 없이 4바이트를 입력하는 테스트
+     // 실행 방법 : 1) abcd 라고 입력하기 2) 가나다라
+    public static void stdioTest1() throws IOException{  
+        
+        int c = System.in.read();         // 바이트 단위 입력
+        // System.out.println(c);         // 1. 읽어온 값은 정수형 저장. 그대로 출력
+        // System.out.println((char)c);   // 2. 아스키코드값은 그대로 출력. UTF-8 한글은 2바이트
+        System.out.write(c);              // 3. 바이트 단위 출력
+        // System.out.flush();            // 필요한 이유 : print, println 자동 flush (출력버퍼에 출력장치로)
+                                          //               write 메소드는 자동 flush 아닙니다. 
+        c= System.in.read();
+        // System.out.println(c);
+        // System.out.println((char)c);
+        System.out.write(c);
+        // System.out.flush();
+
+        c= System.in.read();
+        // System.out.println(c);
+        // System.out.println((char)c);
+        System.out.write(c);
+        // System.out.flush();
+       
+        c= System.in.read();
+        // System.out.println(c);
+        // System.out.println((char)c);
+        System.out.write(c);
+        // System.out.flush();
+
+    }
+
+    public static void stdioTest2() throws IOException{
+        System.out.println("// 위의 코드를 반복문으로 실행하기 //");
+        System.out.println("입력하세요. ↓↓↓↓↓↓↓↓↓");
+        int c;
+        int count=0;
+        while ((c=System.in.read()) != -1) { // 입력종료는 -1. 키보드 ctrl+z
+            System.out.write(c);    // 엔터 입력되면 버퍼를 자동으로 비웁니다.
+            count++;
+        }
+        System.out.println("총 입력바이트 수 : " + count);
+        // 엔터 : 운영체제 윈도우일 때, 아스키코드 CRLF (2바이트)
+        //                  macOS 또는 리눅스일 때, 아스키코드 LF(1바이트)
+
+    }
+
+}
